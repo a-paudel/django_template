f601179f69cee7920ed24fa075728d7c912fdd88
a-paudel <ankur@paudel.me>
2024-01-25 10:08:13 +0000

    Initial push

---
+++ uno.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "unocss";
+
+export default defineConfig({
+    cli: {
+        entry: {
+            outFile: "./static/css/uno.css",
+            patterns: ["./**/*.html", "./**/*.j2", "./**/*.py"]
+        }
+    }
+})
