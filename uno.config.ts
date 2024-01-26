d1ba6da6565c9290e1ada1ed3af18f56d3865bd3
a-paudel <ankur@paudel.me>
2024-01-26 19:17:51 +0000

    rename j2 to html

--- uno.config.ts
+++ uno.config.ts
@@ -1,10 +1,37 @@
-import { defineConfig } from "unocss";
+import { defineConfig, presetUno } from "unocss";
 
 export default defineConfig({
+    presets: [
+        presetUno({
+            dark: "class"
+        }),
+    ],
     cli: {
         entry: {
             outFile: "./static/css/uno.css",
             patterns: ["./**/*.{html,j2,py}"]
         }
-    }
+    },
+    theme: {
+        colors: {
+            "bg": "#f2f2f2",
+            "fg": "#000000",
+            // accent for shadows, borders, etc.
+            "accent": "gray",
+            // muted colors
+            "muted": "gray",
+            // border colros
+            "border": "gray",
+            "input": "gray",
+            // button colors
+            "primary": "steelblue",
+            "primary-fg": "#ffffff",
+            "secondary": "gray",
+            "secondary-fg": "#ffffff",
+            "success": "green",
+            "success-fg": "#ffffff",
+            "danger": "red",
+            "danger-fg": "#ffffff",
+        }
+    },
 })
