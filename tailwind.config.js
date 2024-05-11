2f932a27bcee65aca6a495cd4607c8048b433b18
Ankur Paudel <ankur@paudel.me>
2024-05-11 15:17:28 +0100

    Add tailwind for styling

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+import { addDynamicIconSelectors } from '@iconify/tailwind';
+import daisyui from 'daisyui';
+import typography from "@tailwindcss/typography";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./**/*.{html,py,js,css}",
+    "!./node_modules/**",
+    "!./.venv/**",
+  ],
+  darkMode: "media",
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    typography,
+    daisyui,
+    addDynamicIconSelectors(),
+  ],
+}
+
