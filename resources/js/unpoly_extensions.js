e85940d768ad3e59431b8a60276098e180c1cecf
Ankur Paudel <ankur@paudel.me>
2024-06-19 20:38:23 +0100

    Add unpoly extension to replace url instead of pushing url

--- resources/js/unpoly_extensions.js
+++ resources/js/unpoly_extensions.js
@@ -21,3 +21,31 @@ up.compiler("[up-infinite]", function (element) {
         loadMoreObserver.disconnect()
     }
 })
+
+up.compiler("[up-replace-url]", function (element) {
+    function formHandler() {
+        let method = element.getAttribute("method") ?? "get"
+        if (method.toLowerCase() !== "get") return
+
+        let path = element.getAttribute("action") ?? location.pathname
+        let params = new URLSearchParams(new FormData(element))
+        let url = path + "?" + params.toString()
+
+        history.replaceState(null, "", url)
+    }
+
+    function linkHandler() {
+        let url = element.getAttribute("href")
+        if (!url) return
+
+        history.replaceState(null, "", url)
+    }
+
+    up.on(element, "up:form:submit", formHandler)
+    up.on(element, "up:link:click", linkHandler)
+
+    return function () {
+        up.off(element, "up:form:submit", formHandler)
+        up.off(element, "up:link:click", linkHandler)
+    }
+})
