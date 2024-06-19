d28378ced27b5b5cf2a25465ee9e8dd4b4be80ac
Ankur Paudel <ankur@paudel.me>
2024-06-19 20:31:24 +0100

    Add extension for unpoly for infinite loading

--- resources/js/unpoly_extensions.js
+++ resources/js/unpoly_extensions.js
@@ -1,2 +1,23 @@
 import "unpoly/unpoly"
 import "unpoly/unpoly.css"
+
+
+up.compiler("[up-infinite]", function (element) {
+    let loadMoreObserver = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                up.follow(entry.target)
+            }
+        })
+    }, {
+        root: null,
+        rootMargin: "50%",
+        threshold: 0
+    })
+
+    loadMoreObserver.observe(element)
+
+    return function () {
+        loadMoreObserver.disconnect()
+    }
+})
