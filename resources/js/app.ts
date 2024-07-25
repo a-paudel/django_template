f58dbac56484357319ee2abd2a5d229e72baf0ba
Ankur Paudel <ankur@paudel.me>
2024-07-25 23:25:25 +0100

    crud command add unpoly

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,3 +1,8 @@
 import 'vite/modulepreload-polyfill';
 import "./unpoly_extensions.js"
 import "../css/app.css";
+import Alpine from "alpinejs"
+
+// @ts-ignore
+window.Alpine = Alpine
+Alpine.start()
