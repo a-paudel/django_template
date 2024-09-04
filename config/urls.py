ed9b1f621185a9a31996f212a17e61be04240db3
Ankur Paudel <ankur@paudel.me>
2024-09-04 16:04:17 +0100

    serve media files in debug mode

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf.urls.static import static
 from users.views import (
     LoginView,
     LogoutView,
@@ -45,4 +46,4 @@ urlpatterns = [
     path("rq/", include("django_rq.urls")),
     # add user routes
     path("users/", include((users_paths, "users"), namespace="users")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
